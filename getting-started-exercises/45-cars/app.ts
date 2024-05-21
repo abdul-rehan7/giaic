72ec7b2a858a3ff2bd10185d076ca22d7a89ff13
rehanmirza76 <abdulrehan0317@gmail.com>
2024-05-21 13:10:30 +0500

    commit

--- getting-started-exercises/45-cars/app.ts
+++ getting-started-exercises/45-cars/app.ts
@@ -1,19 +1,21 @@
 /* Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly */
 
-type car = {
-    manufacture: string;
-    model: string;
-    [optional: string]: any;
-}
 
-function createCar(manufacture: string, model: string, optional: Record<string, any>) {
-    return {
-        manufacture,
+
+function createCar(manufacturer:string,model:string,...others:[string,string][]){
+    let car:{[key: string]:any} = {
+        manufacturer,
         model,
-        ...optional
+
+    };
+
+    for (let [key,value] of others){
+        car[key] = value;
     }
+ return car;
+ 
 }
+let myCar = createCar("Honda","Civic",["Shape","Saloon"])
 
-const mycar: car = createCar("Honda", "Civic", { color: "Grey", Year: "2023" })
+console.log(myCar)
 
-console.log(mycar);
